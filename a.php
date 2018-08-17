7b95e253f85fead46e0e6ef7d16528e3c4cfb881
cyt1223 <41795535+cyt1223@users.noreply.github.com>
2018-08-17 20:32:34 +0800

    Add files via upload

---
+++ a.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/17
+ * Time: 19:55
+ */
+if($_GET['a']=='是'){
+    echo '欢迎━(*｀∀´*)ノ亻!<br>';
+}
+
+if($_GET['a']=='否'){
+    '<font size="10" color="red">来自我们的怒视<font>';
+}
+
+
+    echo '<font color="green"><h1>滚滚滚<h1></font>';
+?>
+</body>
+</html>
+
